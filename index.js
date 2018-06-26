d8f54e6ddcdde4e77c2ad3b0c4d57acd7e48d43a
Prarintita Ratanavarinchai <prarintita@gmail.com>
2018-06-26 09:28:01 -0400

    User input can change main header text. Enter key or submit button works

--- index.js
+++ index.js
@@ -3,25 +3,24 @@ const header = document.querySelector('h1');
 const header2 = document.querySelector('#change');
 
 let i = 0;
+let clicked = false; //will not change header again if already clicked on
 const updateText = function() {
-    if (i % 2 === 0) {
+    if (i % 2 === 0 && clicked === false) {
         header.textContent = 'Try again!';
-    } else {
+    } else {   
         header2.textContent = 'Tada!';
     }
     i++;
 };
 
-if (i % 2 === 0) {
-    button.addEventListener('click', updateText);
-} else {
-    button.addEventListener('click', updateText);
-}
+button.addEventListener('click', updateText); //for the first click
+button.addEventListener('click', updateText); //for second click
 
-function newHeaderName() {
+function updateHeader() {
     const newHeader = document.querySelector('#newHeader').value;
-    header.textContent = newHeader;
+    if (newHeader !== "") { //will not change to empty string
+        header.textContent = newHeader;
+        clicked = true;
+    }
+    event.preventDefault(); //will not reset page
 }
-
-// const submitBtn = document.querySelector('#submitBtn');
-// submitBtn.addEventListener('click', newHeaderName);
