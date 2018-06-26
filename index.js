a701007881b173c8704b469985299d2e3b888920
Prarintita Ratanavarinchai <prarintita@gmail.com>
2018-06-25 21:38:45 -0400

    Heading changes when button is clicked

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('yay')
+const button = document.querySelector('button');
+const header = document.querySelector('h1');
+
+let i = 0;
+const updateText = function() {
+    if (i % 2 === 0) {
+        header.textContent = 'Again!';
+    } else {
+        header.textContent = 'Tada!';
+    }
+    i++;
+    console.log(i);
+};
+
+if (i % 2 === 0) {
+    button.addEventListener('click', updateText);
+} else {
+    button.addEventListener('click', updateText);
+}
