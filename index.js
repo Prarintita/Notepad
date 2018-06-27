9d1048411efacec539bb18ef26cb3fcb4ec86480
Prarintita Ratanavarinchai <prarintita@gmail.com>
2018-06-27 15:07:48 -0400

    Change the layout to be a notepad application

--- index.js
+++ index.js
@@ -1,62 +1,27 @@
-const button = document.querySelector('button');
 const header = document.querySelector('h1');
 const header2 = document.querySelector('h2');
 const header3 = document.querySelector('#change');
 
-//user clicks button
-let i = 0;
-let change = false;
-const updateText = function() {
-    if (i % 2 === 0 && change === false) {
-        header.textContent = 'Try again!';
-        change = true;
-    } else {   
-        header3.textContent = 'Tada!';
-    }
-    i++;
-};
-
-button.addEventListener('click', updateText); //for the first click
-button.addEventListener('click', updateText); //for the second click
-
-//waiting for user input
-function userInputHeaders() {
+function listUserInputs() {
     event.preventDefault();
-    
-    //changes main header
+
     const newMainHeader = document.querySelector('#newMainHeader').value;
-    if (newMainHeader !== "") { //will not change to empty string
-        header.textContent = newMainHeader;
-        change = true;
-    }
-
-    //changes 2nd header
     const newSecondHeader = document.querySelector('#newSecondHeader').value;
-    if (newSecondHeader !== "") {
-        header2.textContent = newSecondHeader;
-    }
-
-    listUserInputs(newMainHeader, newSecondHeader);
-
-    document.querySelector('#newMainHeader').value = "";
-    document.querySelector('#newSecondHeader').value = "";
-}
-
-function listUserInputs(newMainHeader, newSecondHeader) {
-    event.preventDefault();
 
     //lists user inputs
-    //Main header
+    //Title of note
     const item1 = document.createElement('li');
     item1.className = "mainHeader";
     item1.textContent = newMainHeader;
     const list1 = document.querySelector("#head");
     list1.appendChild(item1);
     
-    //Second header
-    const item2 = document.createElement('li');
+    //Description of note
+    const item2 = document.createElement('p');
     item2.className = "secondHeader";
     item2.textContent = newSecondHeader;
-    const list2 = document.querySelector("#body");
-    list2.appendChild(item2);
+    list1.appendChild(item2);
+
+    document.querySelector('#newMainHeader').value = "";
+    document.querySelector('#newSecondHeader').value = "";
 }
