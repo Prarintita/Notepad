a13232bc636a5a762b336b5f9dfee5eccfe6351c
Prarintita Ratanavarinchai <prarintita@gmail.com>
2018-06-27 20:32:28 -0400

    Change class names and IDs accordingly

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 const header = document.querySelector('h1');
 const header2 = document.querySelector('h2');
-const header3 = document.querySelector('#change');
 
 function listUserInputs() {
     event.preventDefault();
 
-    const newMainHeader = document.querySelector('#newMainHeader').value;
-    const newSecondHeader = document.querySelector('#newSecondHeader').value;
+    const newTitle = document.querySelector('#newTitle').value;
+    const newDescription = document.querySelector('#newDescription').value;
 
     //lists user inputs
-    //Title of note
-    const item1 = document.createElement('li');
-    item1.className = "mainHeader";
-    item1.textContent = newMainHeader;
-    const list1 = document.querySelector("#head");
-    list1.appendChild(item1);
+    if (newTitle !== "") {
+        //Title of note
+        const item1 = document.createElement('li');
+        item1.className = "title";
+        item1.textContent = newTitle;
+        const list1 = document.querySelector("#head");
+        list1.appendChild(item1);
     
-    //Description of note
-    const item2 = document.createElement('p');
-    item2.className = "secondHeader";
-    item2.textContent = newSecondHeader;
-    list1.appendChild(item2);
+        //Description of note
+        const item2 = document.createElement('p');
+        item2.className = "description";
+        item2.textContent = newDescription;
+        list1.appendChild(item2);
+    }
 
-    document.querySelector('#newMainHeader').value = "";
-    document.querySelector('#newSecondHeader').value = "";
+    //clear input box
+    document.querySelector('#newTitle').value = "";
+    document.querySelector('#newDescription').value = "";
 }
+
